3032274781532b8a68f44a62d63ac11cc82db44d
DanieGu <danie.guenter@gmail.com>
2021-12-03 17:03:38 -0700

    Day3

---
+++ src/AofCode21.Tests/Day3Tests.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AofCode21.Tests
+{
+    [TestClass]
+    public class Day3
+    {
+        [TestMethod]
+        public void Answer1()
+        {
+            var input = File.ReadAllLines("Day3Input.txt");
+
+            var result = AofCode21.Day3.FirstStar(input);
+
+            Assert.Inconclusive($"{result}");
+        }
+
+        [TestMethod]
+        public void Answer1_Test1()
+        {
+            var input = new string[]{
+"00100",
+"11110",
+"10110",
+"10111",
+"10101",
+"01111",
+"00111",
+"11100",
+"10000",
+"11001",
+"00010",
+"01010"
+            };
+
+            var result = AofCode21.Day3.FirstStar(input);
+
+            Assert.AreEqual(198, result);
+        }
+
+        [TestMethod]
+        public void Answer2()
+        {
+            var input = File.ReadAllLines("Day3Input.txt");
+
+            var result = AofCode21.Day3.SecondStar(input);
+
+            Assert.Inconclusive($"{result}");
+        }
+
+        [TestMethod]
+        public void Answer2_Test1()
+        {
+            var input = new string[]{
+"00100",
+"11110",
+"10110",
+"10111",
+"10101",
+"01111",
+"00111",
+"11100",
+"10000",
+"11001",
+"00010",
+"01010"
+            };
+
+            var result = AofCode21.Day3.SecondStar(input);
+
+            Assert.AreEqual(230, result);
+        }
+    }
+}
---
+++ src/AofCode21/Day3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AofCode21
+{
+    public class Day3
+    {
+        public static int FirstStar(IEnumerable<string> input)
+        {
+            var bits = input.Select(s => s.Select(c => c == '1' ? true : false).ToArray()).ToList();
+
+            var gamma = bits.First()
+                .Select((value,index) => bits.Select(b => b[index]).ToArray())
+                .Select(b => b.GroupBy(g => g).OrderByDescending(g => g.Count()).First().Key);
+
+            var epsilon = gamma.Select(b => !b);
+
+            return BitsToInt(gamma) * BitsToInt(epsilon);
+        }
+
+        private static int BitsToInt(IEnumerable<bool> bits)
+        {
+            return Convert.ToInt32(new string(bits.Select(b => b ? '1' : '0').ToArray()), 2);
+        }
+
+        public static int SecondStar(IEnumerable<string> input)
+        {
+            var bits = input.Select(s => s.Select(c => c == '1' ? true : false).ToArray()).ToList();
+
+            var oRating = Filter(bits, 0, positive:true).Single();
+            var co2Rating = Filter(bits, 0, positive:false).Single();
+
+            return BitsToInt(oRating) * BitsToInt(co2Rating);
+        }
+
+        private static IEnumerable<bool[]> Filter(IEnumerable<bool[]> bits, int index, bool positive)
+        {
+            bits = bits.GroupBy(bs => bs[index]).OrderBy(g => g.Count()).ThenBy(g => g.Key).Skip(Convert.ToInt32(positive)).First();
+            if(bits.Count() > 1)
+                return Filter(bits, index + 1, positive);
+            
+            return bits;
+        }        
+    }
+}
