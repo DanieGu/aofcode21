ceb96dd021d6d6bcffbb8478c0b5aa1fc66d8697
DanieGu <danie.guenter@gmail.com>
2021-12-09 15:27:08 -0700

    .net 6 and cleanup

---
+++ src/AofCode21/DayTemplate.cs
@@ -0,0 +1,18 @@
+namespace AofCode21;
+
+using System;
+using System.Collections.Generic;
+
+//https://adventofcode.com/2021/day/
+public class Day
+{
+    public static Int64 FirstStar(IEnumerable<string> input)
+    {
+        return 0;
+    }
+
+    public static Int64 SecondStar(IEnumerable<string> input)
+    {
+        return 0;
+    }
+}
